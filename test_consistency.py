4bd45c80a615ac2094f38ade9d13df5a7fbd7019
kaibo <ktang@unc.edu>
2025-05-16 08:43:14 -0600

    add ci/cd to check consistency between python and julia implementations

--- test_consistency.py
+++ test_consistency.py
@@ -1,5 +1,4 @@
 from juliacall import Main as jl
-import matlab.engine
 import numpy as np
 from relaxationColor import relaxationColorMap
 
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     upLev = 2000.0
 
     # Julia 
-    jl.include("relaxationColor.jl")
+    jl.include("RelaxationColor.jl")
     imClip_julia, cmap_julia = jl.relaxationColorMap(maptype, im, loLev, upLev)
     imClip_julia = np.asarray(imClip_julia)
     cmap_julia = np.asarray([[x.r, x.g, x.b] for x in cmap_julia])
@@ -22,18 +21,12 @@ if __name__ == "__main__":
     imClip_python = np.asarray(imClip_python)
     cmap_python = cmap_python.colors
 
-    # MATLAB
-    eng = matlab.engine.start_matlab()
-    eng.addpath(eng.genpath("./"))
-    imClip_matlab, cmap_matlab = eng.relaxationColorMap(maptype, im, loLev, upLev, nargout=2)
-    imClip_matlab = np.asarray(imClip_matlab)
-    cmap_matlab = np.asarray(cmap_matlab)
-
-    if not np.allclose(imClip_julia, imClip_python):
-        print("imClip mismatch between Julia and Python")
-    if not np.allclose(cmap_julia, cmap_python):
-        print("cmap mismatch between Julia and Python")
-    if not np.allclose(imClip_julia, imClip_matlab):
-        print("imClip mismatch between Julia and MATLAB")
-    if not np.allclose(cmap_julia, cmap_matlab):
-        print("cmap mismatch between Julia and MATLAB")
+    if np.allclose(imClip_julia, imClip_python):
+        print("imClip match between Julia and Python")
+    else:
+        raise ValueError("imClip mismatch between Julia and Python")
+    
+    if np.allclose(cmap_julia, cmap_python):
+        print("cmap match between Julia and Python")
+    else:
+        raise ValueError("cmap mismatch between Julia and Python")
